60a27f878234424179be67e7223d86bb9f00b674
Thomas <nyumat18@gmail.com>
2023-10-25 21:34:11 -0700

    👑 Putting everything together

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+import dotenv from "dotenv"
 import express from "express"
+import mongoose from "mongoose"
 
-const app = express()
-const PORT = 8080
+import logger from "./middleware/logger.js"
 
+import userRoutes from "./routes/users.js"
+
+dotenv.config({ path: "./config/.env.local" });
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const MONGODB_ACCESS = process.env.MONGODB_ACCESS;
+
+// Middleware
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/users", userRoutes)
+
+// Hello World
 app.get("/", (req, res) => {
     res.send("Hello BeavsAI")
 })
 
+// Connect to MongoDB
+mongoose.connect(MONGODB_ACCESS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("[BeavsAI-Backend] Successfully connected to MongoDB.")
+}).catch((err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`BeavsAI Backend server running on port ${PORT}`)
+    console.log(`[BeavsAI-Backend] Server running on port: ${PORT}`)
 })
